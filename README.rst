500a79e7d685b6106ae2e8e840f99d03cee21a0d
Tom Ritchford <tom@swirly.com>
2020-04-09 18:20:24 +0200

    Document safer module and functions

--- README.rst
+++ README.rst
@@ -1,7 +1,14 @@
 ✏️safer ✏️
 ----------------------
 
-Safely write or print to a file, leaving it unchanged if something goes wrong.
+Safely write or print to a file, leaving it unchanged if there's an exception
+
+Writes happen on a temporary file, which is only copied over the original file
+when the context completes successfully.
+
+This means that ``safer`` will temporarily use as much disk space as the old
+file and the new file put together.
+
 
 Example:
 
@@ -13,13 +20,16 @@ Example:
        for line in source():
           fp.write('this and that')
 
+       if CHANGED_MY_MIND:
+           # filename will be unchanged
+           raise ValueError
+
+   # or
+
    with safer.printer(filename) as print:
        print('this', 'and', 'that')
        print('two', 'lines', sep='\n', end='\n---\n')
 
-Writes occur on a temporary file, which is only copied over the original file
-when the block completes successfully, so ``safer`` will temporarily use
-as much disk space as the old file and the new file put together.
-
-This is great for writing any files which you don't want to get partially
-overwritten if something goes wrong in the writing process.
+       if CHANGED_MY_MIND:
+           # filename will be unchanged
+           raise ValueError
--- safer.py
+++ safer.py
@@ -1,3 +1,40 @@
+""""
+Safely write or print to a file, leaving it unchanged if there's an exception.
+
+Works on Python versions 2.7 through 3.8 and likely beyond.
+
+Writes happen on a temporary file, which is only copied over the original file
+after the context completes successfully.  This temporarily uses as much disk
+space as the old file and the new file put together.
+
+``safer.open`` example:
+
+.. code-block:: python
+
+   import safer
+
+   with safer.open(filename, 'w') as fp:
+       for line in source():
+          fp.write('this and that')
+
+       if CHANGED_MY_MIND:
+           raise ValueError
+           # Contents of `filename` will be unchanged
+
+``safer.printer`` example:
+
+.. code-block:: python
+
+   with safer.printer(filename) as print:
+       print('this', 'and', 'that')
+       print('two', 'lines', sep='\n', end='\n---\n')
+
+       if CHANGED_MY_MIND:
+           raise ValueError
+           # Contents of `filename` will be unchanged
+
+"""
+
 from __future__ import print_function
 import contextlib
 import functools
@@ -16,6 +53,25 @@ def open(
     delete_failures=True,
     suffix=SUFFIX,
 ):
+    """A context that yields a stream like built-in open(), but undoes any
+    changes to the file if there's an exception.
+
+    Arguments:
+      file:
+        Path to the file to be opened
+
+      mode:
+        Mode string passed to built-in ``open``
+
+      create_parents:
+        If True, all parent directories are automatically created
+
+      delete_failures:
+        Are partial files deleted if there's an Exception?
+
+      suffix:
+        File suffix to use for temporary files
+    """
     copy = '+' in mode or 'a' in mode
     read_only = not copy and 'r' in mode
 
@@ -52,6 +108,31 @@ def open(
 
 
 @contextlib.contextmanager
-def printer(file, create_parents=False, delete_failures=True, suffix=SUFFIX):
-    with open(file, 'w', create_parents, delete_failures, suffix) as fp:
+def printer(
+        file,
+        mode='w',
+        create_parents=False,
+        delete_failures=True,
+        suffix=SUFFIX
+):
+    """A context that yields a print function that prints to a file,
+    but undoes any changes to the file if there's an exception.
+
+    Arguments:
+      file:
+        Path to the file to be opened
+
+      mode:
+        Mode string passed to built-in ``open``
+
+      create_parents:
+        If True, all parent directories are automatically created
+
+      delete_failures:
+        Are partial files deleted if there's an Exception?
+
+      suffix:
+        File suffix to use for temporary files
+    """
+    with open(file, mode, create_parents, delete_failures, suffix) as fp:
         yield functools.partial(print, file=fp)
