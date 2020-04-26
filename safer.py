a9721286178e26813e0d79c492f338f2b4336bb7
Tom Ritchford <tom@swirly.com>
2020-04-27 00:22:43 +0200

    New version 2.0.5

--- safer.py
+++ safer.py
@@ -80,7 +80,7 @@ import tempfile
 import traceback
 import warnings
 
-__version__ = '2.0.4'
+__version__ = '2.0.5'
 __all__ = 'open', 'printer', 'writer'
 _raw_open = __builtins__['open']
 IS_PY2 = platform.python_version() < '3'
