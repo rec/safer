4abbf2c36d9317a3286f3835fec2bea8896f9c64
Tom Ritchford <tom@swirly.com>
2020-05-06 13:19:25 +0200

    safer.writer() now returns a stream

--- test_safer.py
+++ test_safer.py
@@ -283,53 +283,53 @@ class TestWriter(TestCase):
 
     def test_callable(self):
         results = []
-        with safer.writer(results.append) as write:
-            write('abc')
-            write('d')
+        with safer.writer(results.append) as fp:
+            fp.write('abc')
+            fp.write('d')
         assert results == ['abcd']
 
     def test_callable_error(self):
         results = []
         with self.assertRaises(ValueError):
-            with safer.writer(results.append) as write:
-                write('abc')
-                write('d')
+            with safer.writer(results.append) as fp:
+                fp.write('abc')
+                fp.write('d')
                 raise ValueError
         assert results == []
 
     def test_file(self):
-        with safer.open(self.filename, 'w') as fp:
-            fp.write('one')
-            with safer.writer(fp) as write:
-                write('two')
-                write('three')
-            fp.write('four')
+        with safer.open(self.filename, 'w') as fp1:
+            fp1.write('one')
+            with safer.writer(fp1) as fp2:
+                fp2.write('two')
+                fp2.write('three')
+            fp1.write('four')
         assert self.filename.read_text() == 'onetwothreefour'
 
     def test_file_error(self):
-        with safer.open(self.filename, 'w') as fp:
-            fp.write('one')
+        with safer.open(self.filename, 'w') as fp1:
+            fp1.write('one')
             with self.assertRaises(ValueError):
-                with safer.writer(fp) as write:
-                    write('two')
-                    write('three')
+                with safer.writer(fp1) as fp2:
+                    fp2.write('two')
+                    fp2.write('three')
                     raise ValueError
-            fp.write('four')
+            fp1.write('four')
         assert self.filename.read_text() == 'onefour'
 
     def test_socket(self):
         sock = socket()
-        with safer.writer(sock) as write:
-            write(b'one')
-            write(b'two')
+        with safer.writer(sock) as fp:
+            fp.write(b'one')
+            fp.write(b'two')
         assert sock.items == [b'onetwo']
 
     def test_socket_error(self):
         sock = socket()
         with self.assertRaises(ValueError):
-            with safer.writer(sock) as write:
-                write(b'one')
-                write(b'two')
+            with safer.writer(sock) as fp:
+                fp.write(b'one')
+                fp.write(b'two')
                 raise ValueError
         assert sock.items == []
 
